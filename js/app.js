895dafc281667b99b9fb159f087146faaad72fef
ancaagp <anca.agapi@gmail.com>
2020-02-11 21:24:39 -0800

    WIP css updated and js functions

--- js/app.js
+++ js/app.js
@@ -2,27 +2,112 @@ console.log("hola");
 
 // CONSTANTS
 
-const words = [];
-let currWord = ""; // word that the player has to guess
-let correctLetters = ["L", "R", "T"];
+const words = ["McCormick", "Broflovski", "Barbrady", "TweekTweak"];
+let currWord = "LETTER"; // word that the player has to guess
+let correctLetters = [];
 let wrongLetters = [];
 let numberOfTries = 5;
-
 
 // STATE VARIABLES
 
 // CACHED ELEMENTS
 
+let btnStart = document.getElementById("btnStart");
+let keyboard = document.getElementById("keyContainer")
+
 // EVENT LISTENERS
+
+keyboard.addEventListener("click", selectLetter);
+btnStart.addEventListener("click", startGame);
+
+
+
+function isLetterUsed (letter) {
+    return correctLetters.includes(letter) || wrongLetters.includes(letter); // returns true or false
+}
+
+function selectLetter (event) {
+    let selectedLetter = event.target.textContent;
+    let selectedElement = event.target;
+    if (isLetterUsed(selectedLetter) !== true) {
+        checkForMatch(selectedLetter, selectedElement);
+    }
+} 
+
+
+function countLetter (word, letter) {
+    // checks if the letter is in the word - returns 0 or >=1
+    let count = 0;
+    for (let i = 0; i < word.length; i++) {
+        if (letter === word[i]) {
+            count ++;
+        }
+    } 
+    return count;
+};
+
+function updateHiddenWord (letter) {
+    // adds correct letter to hidden word
+    // hint: https://www.w3schools.com/jsref/met_table_insertrow.asp
+}
+
+function checkForMatch (letter, element) {
+    let count = countLetter(currWord,letter);
+    if (count > 0) {
+        for (let i = 0; i < count; i++) {
+            correctLetters.push(letter);
+        }
+        updateHiddenWord(letter);
+        checkForWin();
+    } else {
+        wrongLetters.push(letter);
+        // update meteorite
+        // check for loss
+    }
+    disableKey(letter, element);
+}
+
+function checkForWin () {
+    if (currWord.length === correctLetters.length) {
+        console.log("You won");
+    }
+    console.log(correctLetters);
+}
+
+function disableKey (letter, element) {
+    element.style.color = "lightgrey";
+    element.style.border = "2px solid lightgrey";
+}
+
+function shuffleWords (arrayWords) {
+    for (let i = arrayWords.length; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [arrayWords[i], arrayWords[j]] = [arrayWords[j], arrayWords[i]];
+
+    }
+}
+
+function startGame () {
+    shuffleWords (words);
+    currWord = words.pop().toUpperCase();
+    console.log(currWord);
+    
+    let wordLength = currWord.length;
+    updateHiddenWord();
+    numberOfTries = Math.floor(wordLength - 0.3 * wordLength);
+}
+
+
+
+
+
+/* -----------------------------------------
 
 // FUNCTIONS
 
-hasLetter (word, letter) {
-    // checks if the letter is in the word - boolean
-};
 
 calculateTries (word) {
-    // returns number of tries for a givem word
+    // returns number of tries for a given word
 };
 
 randomizeWord (arrayWords) {
@@ -34,17 +119,13 @@ selectLetter (letter) {
     // adds letter to wrong or correct array
 }
 
-updateHiddenWord () {
-    // adds correct letter to hidden word
-}
+
 
 updateWrongLetter () {
     // adds letter to array and crosses on keyboard
 }
 
-checkForWin () {
-    // checks if player won
-}
+
 
 checkForLoss () {
     // checks is player lost
@@ -52,12 +133,23 @@ checkForLoss () {
 
 // game starts:
 // randomize word
+
+
+
 // calculate tries based on word length
+
+
 // store tries in numberOfTries
+
+
 // store randomized word in currWord
+
 
 // during game:
 // player selects letter
+
+
+
 // letter is saved in wrong or right array
 // hidden word is updated or meteoroid moves closer to kenny
 // check for win (word length and character match)
@@ -67,3 +159,6 @@ checkForLoss () {
 // Extras:
 // new game
 // score
+
+
+*/
